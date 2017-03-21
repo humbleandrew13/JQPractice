1a99b998836517f7a48e20caad2e2ea7a4e7b570
Andy Humble <humbleandrew13@gmail.com>
2017-03-21 05:33:12 -0400

    initial

---
+++ JQPractice/practice.js
@@ -0,0 +1,15 @@
+﻿$("#btn").click(function () {
+    $("#goAway").slideDown("slow");
+});
+
+$("#btn2").click(function () {
+    $("#goAway").slideUp("slow");
+});
+
+$(document).ready(function () {
+    if($("#par").text() == "A paragraph")
+    {
+        alert($("#myDiv").html());
+        alert($("#inBox").val());
+    }
+})
